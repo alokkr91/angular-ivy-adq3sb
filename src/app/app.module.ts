1778c77dac6c435046cf46ced5d2f26bdaef3754
alokkr91 <contactalok.kr@gmail.com>
2022-07-15 00:01:17 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,19 @@ import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path:'products/:productId', component:ProductDetailsComponent},
+]),
     MatIconModule,
     MatButtonModule,
+
   ],
 
   declarations: [

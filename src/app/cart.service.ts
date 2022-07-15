557143dcfc7a5b64dfccce254cee32c37650bb20
alokkr91 <contactalok.kr@gmail.com>
2022-07-16 01:00:54 +0530

    2 files modified, 1 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+
+@Injectable()
+export class CartService {
+  items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+  getItems() {
+    return this.items;
+  }
+  clearToCart() {
+    this.items = [];
+    return this.items;
+  }
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { products, Product } from '../product';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,12 +12,20 @@ import { products, Product } from '../product';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute, private _location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private _location: Location,
+    private _cartService: CartService
+  ) {}
 
   ngOnInit() {
     var pramRoute = this.route.snapshot.paramMap;
     var prodId = Number(pramRoute.get('productId'));
     this.product = products.find((x) => x.id === prodId);
+  }
+  addToCart(pProduct: Product) {
+    this._cartService.addToCart(pProduct);
+    window.alert('Your product has been added to the cart!');
   }
   backClicked() {
     this._location.back();

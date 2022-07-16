768388607025b11466af8c75ae37b33c95e3d6c1
alokkr91 <contactalok.kr@gmail.com>
2022-07-16 13:23:06 +0530

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
+  providers: [CartService],
 })
 export class CartComponent implements OnInit {
-  items = this.cartService.getItems();
   constructor(private cartService: CartService) {}
-
-  ngOnInit() {}
+  items = this.cartService.getItems();
+  ngOnInit() {
+    window.alert(JSON.stringify(this.cartService.getItems()));
+  }
 }
